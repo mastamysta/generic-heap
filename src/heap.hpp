b72a20949c5040cbcdcb96e9dd4c3fc3f47a1fc7
mastamysta <benjamin-read@hotmail.co.uk>
2024-10-23 19:53:38 +0100

    Implement heap insert operation.

--- src/heap.hpp
+++ src/heap.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <cstdint>
 
 template <typename ValueType>
 class Heap
@@ -8,6 +9,42 @@ class Heap
 public:
     using value_type = ValueType;
 
+    Heap()
+    {
+        // 1-indexed array is helpful for heap structure, but beware! Doing this in the
+        // constructor constrains the value_type to default-constructible types only.
+        data = std::vector<value_type>(1);
+    }
+
+    auto insert(value_type v) -> void
+    {
+        data.push_back(v);
+        heapify(data.size() - 1);
+    }
+
 private:
     std::vector<value_type> data;
+
+    auto heapify(std::size_t item) -> void
+    {
+        while (true)
+        {
+            std::size_t parent = -1;
+
+            if (item & 0x01) // if odd
+                parent = (item & 0x01) >> 1; // Parent is sub 1, divide by two
+            else
+                parent = item >> 1; // Just divide by two
+
+            if (data[item] > data[parent])
+                std::swap(data[item], data[parent]);
+            else
+                break;
+
+            item = parent;
+
+            if (item == 1)
+                break;
+        }
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,10 @@ int main()
 {
     Heap<int> h;
 
+    h.insert(1);
+    h.insert(2);
+    h.insert(100);
+    h.insert(50);
+
     return 0;
 }
